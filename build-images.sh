18eb6c9fa2ad3a44008a461734dece15989430e8
Andrew Azores <aazores@redhat.com>
2023-06-15 14:24:14 -0400

    prune after build

--- build-images.sh
+++ build-images.sh
@@ -13,3 +13,5 @@ podman build -t quay.io/andrewazores/quarkus-quickstart-hibernate:jvm-latest -f 
 
 ./mvnw clean package -Dnative -DskipTests -Dquarkus.native.monitoring=jfr,jmxserver,jmxclient,jvmstat
 podman build -t quay.io/roberttoyonaga/jmx:cryostatquarkus -f src/main/docker/Dockerfile.native .
+
+podman image prune -f
