93f2765928c694e119ea4c16f83f6fa5a5f88b4e
Andrew Azores <aazores@redhat.com>
2023-06-15 17:03:09 -0400

    ensure base images are updated before build

--- build-images.sh
+++ build-images.sh
@@ -9,9 +9,11 @@ if [ -z "${GRAALVM_HOME}" ]; then
 fi
 
 ./mvnw clean package
+podman pull registry.access.redhat.com/ubi8/openjdk-11:1.15
 podman build -t quay.io/andrewazores/quarkus-quickstart-hibernate:jvm-latest -f src/main/docker/Dockerfile.jvm .
 
 ./mvnw clean package -Dnative -DskipTests -Dquarkus.native.monitoring=jfr,jmxserver,jmxclient,jvmstat
+podman pull registry.access.redhat.com/ubi9/ubi-minimal:9.2
 podman build -t quay.io/andrewazores/quarkus-quickstart-hibernate:native-latest -f src/main/docker/Dockerfile.native .
 
 podman image prune -f
