09b3c572dd302124efd9d27d964fd4cd03fda9ca
Andrew Azores <aazores@redhat.com>
2023-06-21 12:08:32 -0400

    add config for third sample app, JVM mode with agents included

--- demo.bash
+++ demo.bash
@@ -8,9 +8,18 @@ mapfile -t COMPOSE < <(ls -1 ./compose-*.yml)
 flags=$(printf -- "-f %s " "${COMPOSE[@]}")
 
 teardown() {
-    docker-compose $flags down --volumes --remove-orphans 
+  if podman volume exists cryostat_quarkus_demo; then
+    podman volume rm -f cryostat_quarkus_demo
+  fi
+  docker-compose $flags down --volumes --remove-orphans
+  rm -f "${tmp}"
 }
 teardown
 trap teardown EXIT
+
+vol="$(podman volume create cryostat_quarkus_demo)"
+tmp="$(mktemp)"
+tar cf "${tmp}" extras/*.jar
+podman volume import "${vol}" "${tmp}"
 
 docker-compose $flags up
